3464c97298fbffb0c68eac2a12cd304cb370d22b
tematrkv <tatarnikov.stud@mail.ru>
2025-03-07 10:17:17 +0700

    Репозиторий, ViewModel и DetailViewModel

--- app/src/main/java/com/example/wewatch/MainActivity.kt
+++ app/src/main/java/com/example/wewatch/MainActivity.kt
@@ -6,12 +6,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var addButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        addButton = findViewById(R.id.buttonAdd)
+        addButton.setOnClickListener(){
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
